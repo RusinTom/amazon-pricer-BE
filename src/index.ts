f6100a86ed84e66e99e5bcd600052fd6a21a9b91
RusinTomasz <tomaszrusin90@gmail.com>
2022-04-09 11:58:17 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
